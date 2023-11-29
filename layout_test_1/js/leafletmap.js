054f69e72600660277cc5a47523b5a79ab5f1bd4
Gianfranco Di Pietro <gianfrancodipietro@gmail,com>
2023-11-29 13:08:43 +0100

    Cloned demo map on layout_test_1 left area

--- layout_test_1/js/leafletmap.js
+++ layout_test_1/js/leafletmap.js
@@ -1,33 +1,443 @@
-// Variabile MAP con le coordinate del centro della mappa
+
+// // Vecchia versione
+
+// // Variabile MAP con le coordinate del centro della mappa
+// var map = L.map('map', {
+//     zoomControl:true, maxZoom:16, minZoom:1
+// })
+// map.setView([37, 14], 6);
+
+// map.attributionControl.setPrefix('CCBYSA &middot; <a href="http://gianfrancodp.github.io">GDP</a> &middot; <a href="https://leafletjs.com" title="A JS library for interactive maps">Leaflet</a> &middot;');
+
+
+// // Crea un nuovo "pane" (strato) per la mappa chiamato 'pane_GoogleSat_0'
+// map.createPane('pane_GoogleSat_0');
+
+// // Imposta l'indice Z del pane a 400. L'indice Z determina l'ordine di sovrapposizione degli elementi: 
+// // gli elementi con un indice Z più alto sovrapporranno quelli con un indice Z più basso.
+// map.getPane('pane_GoogleSat_0').style.zIndex = 400;
+
+// // Crea un nuovo layer di tile (piastrelle) utilizzando le mappe satellitari di Google
+// var layer_GoogleSat_0 = L.tileLayer('https://mt1.google.com/vt/lyrs=s&x={x}&y={y}&z={z}', {
+//     // Assegna il layer al pane creato in precedenza
+//     pane: 'pane_GoogleSat_0',
+//     // Imposta l'opacità del layer a 1 (completamente opaco)
+//     opacity: 1.0,
+//     // Attributi per il layer
+//     attribution: 'google.com',
+//     // Imposta il livello di zoom minimo e massimo per il layer
+//     minZoom: 1,
+//     maxZoom: 16,
+//     // Imposta il livello di zoom nativo minimo e massimo per il layer
+//     minNativeZoom: 0,
+//     maxNativeZoom: 18
+// });
+
+// // Aggiunge il layer alla mappa
+// map.addLayer(layer_GoogleSat_0);
+
+// prova recupero dati da file geojson
+fetch('data/CartaGeoTEST.geojson')
+    .then(response => response.json())
+    .then(data => {
+        jsonDATA = data;
+        // todo add to map
+        console.log(jsonDATA);
+    })
+    .catch(error => console.error(error));
+
+
+//// script file originale
 var map = L.map('map', {
     zoomControl:true, maxZoom:16, minZoom:1
 })
-map.setView([37, 14], 6);
 
-map.attributionControl.setPrefix('CCBYSA &middot; <a href="http://gianfrancodp.github.io">GDP</a> &middot; <a href="https://leafletjs.com" title="A JS library for interactive maps">Leaflet</a> &middot;');
-
-// Crea un nuovo "pane" (strato) per la mappa chiamato 'pane_GoogleSat_0'
+var hash = new L.Hash(map);
+map.attributionControl.setPrefix('CCBYSA &middot; Gianfranco Di Pietro &middot; <a href="https://leafletjs.com" title="A JS library for interactive maps">Leaflet</a> &middot;');
+var autolinker = new Autolinker({truncate: {length: 30, location: 'smart'}});
+var bounds_group = new L.featureGroup([]);
+function setBounds() {
+    if (bounds_group.getLayers().length) {
+        map.fitBounds(bounds_group.getBounds());
+    }
+    map.setMaxBounds(map.getBounds());
+}
+// Adding Google Satellite basemap    
 map.createPane('pane_GoogleSat_0');
-
-// Imposta l'indice Z del pane a 400. L'indice Z determina l'ordine di sovrapposizione degli elementi: 
-// gli elementi con un indice Z più alto sovrapporranno quelli con un indice Z più basso.
 map.getPane('pane_GoogleSat_0').style.zIndex = 400;
-
-// Crea un nuovo layer di tile (piastrelle) utilizzando le mappe satellitari di Google
 var layer_GoogleSat_0 = L.tileLayer('https://mt1.google.com/vt/lyrs=s&x={x}&y={y}&z={z}', {
-    // Assegna il layer al pane creato in precedenza
     pane: 'pane_GoogleSat_0',
-    // Imposta l'opacità del layer a 1 (completamente opaco)
     opacity: 1.0,
-    // Attributi per il layer
-    attribution: 'google.com',
-    // Imposta il livello di zoom minimo e massimo per il layer
+    attribution: '',
     minZoom: 1,
     maxZoom: 16,
-    // Imposta il livello di zoom nativo minimo e massimo per il layer
     minNativeZoom: 0,
     maxNativeZoom: 18
 });
 
-// Aggiunge il layer alla mappa
+layer_GoogleSat_0;
 map.addLayer(layer_GoogleSat_0);
+function pop_CartaGeologica_1(feature, layer) {
+    var popupContent = '<table>\
+            <tr>\
+                <th scope="row">Litotipo: </th>\
+                <td>' + (feature.properties['Litotipo'] !== null ? autolinker.link(feature.properties['Litotipo'].toLocaleString()) : '') + '</td>\
+            </tr>\
+        </table>';
+        // Here there a sample of table in popup
+    // var popupContent = '<table>\
+    //         <tr>\
+    //             <th scope="row">fid</th>\
+    //             <td>' + (feature.properties['fid'] !== null ? autolinker.link(feature.properties['fid'].toLocaleString()) : '') + '</td>\
+    //         </tr>\
+    //         <tr>\
+    //             <td colspan="2">' + (feature.properties['Id'] !== null ? autolinker.link(feature.properties['Id'].toLocaleString()) : '') + '</td>\
+    //         </tr>\
+    //         <tr>\
+    //             <th scope="row">Litotipo</th>\
+    //             <td>' + (feature.properties['Litotipo'] !== null ? autolinker.link(feature.properties['Litotipo'].toLocaleString()) : '') + '</td>\
+    //         </tr>\
+    //         <tr>\
+    //             <td colspan="2">' + (feature.properties['3yhw'] !== null ? autolinker.link(feature.properties['3yhw'].toLocaleString()) : '') + '</td>\
+    //         </tr>\
+    //     </table>';
+
+    
+    layer.bindPopup(popupContent, {maxHeight: 400});
+}
+
+// Carta Geologica 
+// TODO make this section in a separate file or in a separate function
+function style_CartaGeologica_1_0(feature) {
+    switch(String(feature.properties['Litotipo'])) {
+        case 'Arenarie silicoclastiche':
+            return {
+        pane: 'pane_CartaGeologica_1',
+        opacity: 0.5,
+        color: 'rgba(35,35,35,1.0)',
+        dashArray: '',
+        lineCap: 'butt',
+        lineJoin: 'miter',
+        weight: 1.0, 
+        fill: true,
+        fillOpacity: 0.5,
+        fillColor: 'rgba(218,52,55,1.0)',
+        interactive: true,
+    }
+            break;
+        case 'Argille marnose':
+            return {
+        pane: 'pane_CartaGeologica_1',
+        opacity: 0.5,
+        color: 'rgba(35,35,35,1.0)',
+        dashArray: '',
+        lineCap: 'butt',
+        lineJoin: 'miter',
+        weight: 1.0, 
+        fill: true,
+        fillOpacity: 0.5,
+        fillColor: 'rgba(224,80,142,1.0)',
+        interactive: true,
+    }
+            break;
+        case 'Calcari a Porites':
+            return {
+        pane: 'pane_CartaGeologica_1',
+        opacity: 0.5,
+        color: 'rgba(35,35,35,1.0)',
+        dashArray: '',
+        lineCap: 'butt',
+        lineJoin: 'miter',
+        weight: 1.0, 
+        fill: true,
+        fillOpacity: 0.5,
+        fillColor: 'rgba(39,216,216,1.0)',
+        interactive: true,
+    }
+            break;
+        case 'Calcari brecciati evaporitici':
+            return {
+        pane: 'pane_CartaGeologica_1',
+        opacity: 0.5,
+        color: 'rgba(35,35,35,1.0)',
+        dashArray: '',
+        lineCap: 'butt',
+        lineJoin: 'miter',
+        weight: 1.0, 
+        fill: true,
+        fillOpacity: 0.5,
+        fillColor: 'rgba(20,213,126,1.0)',
+        interactive: true,
+    }
+            break;
+        case 'Detrito':
+            return {
+        pane: 'pane_CartaGeologica_1',
+        opacity: 0.5,
+        color: 'rgba(35,35,35,1.0)',
+        dashArray: '',
+        lineCap: 'butt',
+        lineJoin: 'miter',
+        weight: 1.0, 
+        fill: true,
+        fillOpacity: 0.5,
+        fillColor: 'rgba(227,164,119,1.0)',
+        interactive: true,
+    }
+            break;
+        case 'Frane':
+            return {
+        pane: 'pane_CartaGeologica_1',
+        opacity: 0.5,
+        color: 'rgba(35,35,35,1.0)',
+        dashArray: '',
+        lineCap: 'butt',
+        lineJoin: 'miter',
+        weight: 1.0, 
+        fill: true,
+        fillOpacity: 0.5,
+        fillColor: 'rgba(227,33,201,1.0)',
+        interactive: true,
+    }
+            break;
+        case 'Gneiss occhiadini':
+            return {
+        pane: 'pane_CartaGeologica_1',
+        opacity: 0.5,
+        color: 'rgba(35,35,35,1.0)',
+        dashArray: '',
+        lineCap: 'butt',
+        lineJoin: 'miter',
+        weight: 1.0, 
+        fill: true,
+        fillOpacity: 0.5,
+        fillColor: 'rgba(204,181,48,1.0)',
+        interactive: true,
+    }
+            break;
+        case 'Gneiss tonalitici':
+            return {
+        pane: 'pane_CartaGeologica_1',
+        opacity: 1,
+        color: 'rgba(35,35,35,1.0)',
+        dashArray: '',
+        lineCap: 'butt',
+        lineJoin: 'miter',
+        weight: 1.0, 
+        fill: true,
+        fillOpacity: 0.5,
+        fillColor: 'rgba(179,98,214,1.0)',
+        interactive: true,
+    }
+            break;
+        case 'Paragneiss migmatitici':
+            return {
+        pane: 'pane_CartaGeologica_1',
+        opacity: 0.5,
+        color: 'rgba(35,35,35,1.0)',
+        dashArray: '',
+        lineCap: 'butt',
+        lineJoin: 'miter',
+        weight: 1.0, 
+        fill: true,
+        fillOpacity: 0.5,
+        fillColor: 'rgba(55,212,76,1.0)',
+        interactive: true,
+    }
+            break;
+        case 'Skarns':
+            return {
+        pane: 'pane_CartaGeologica_1',
+        opacity: 0.5,
+        color: 'rgba(35,35,35,1.0)',
+        dashArray: '',
+        lineCap: 'butt',
+        lineJoin: 'miter',
+        weight: 1.0, 
+        fill: true,
+        fillOpacity: 0.5,
+        fillColor: 'rgba(145,235,107,1.0)',
+        interactive: true,
+    }
+            break;
+        case 'Spiagge e alluvioni':
+            return {
+        pane: 'pane_CartaGeologica_1',
+        opacity: 1,
+        color: 'rgba(35,35,35,1.0)',
+        dashArray: '',
+        lineCap: 'butt',
+        lineJoin: 'miter',
+        weight: 1.0, 
+        fill: true,
+        fillOpacity: 0.5,
+        fillColor: 'rgba(185,216,99,1.0)',
+        interactive: true,
+    }
+            break;
+        case 'Terrazzi marini':
+            return {
+        pane: 'pane_CartaGeologica_1',
+        opacity: 1,
+        color: 'rgba(35,35,35,1.0)',
+        dashArray: '',
+        lineCap: 'butt',
+        lineJoin: 'miter',
+        weight: 1.0, 
+        fill: true,
+        fillOpacity: 0.5,
+        fillColor: 'rgba(21,52,231,1.0)',
+        interactive: true,
+    }
+            break;
+        case 'Trubi':
+            return {
+        pane: 'pane_CartaGeologica_1',
+        opacity: 1,
+        color: 'rgba(35,35,35,1.0)',
+        dashArray: '',
+        lineCap: 'butt',
+        lineJoin: 'miter',
+        weight: 1.0, 
+        fill: true,
+        fillOpacity: 0.5,
+        fillColor: 'rgba(94,50,206,1.0)',
+        interactive: true,
+    }
+            break;
+        default:
+            return {
+        pane: 'pane_CartaGeologica_1',
+        opacity: 1,
+        color: 'rgba(35,35,35,1.0)',
+        dashArray: '',
+        lineCap: 'butt',
+        lineJoin: 'miter',
+        weight: 1.0, 
+        fill: true,
+        fillOpacity: 0.5,
+        fillColor: 'rgba(125,172,208,1.0)',
+        interactive: true,
+    }
+            break;
+    }
+}
+map.createPane('pane_CartaGeologica_1');
+map.getPane('pane_CartaGeologica_1').style.zIndex = 401;
+map.getPane('pane_CartaGeologica_1').style['mix-blend-mode'] = 'normal';
+var layer_CartaGeologica_1 = new L.geoJson(json_CartaGeologica_1, {
+    attribution: '',
+    interactive: true,
+    dataVar: 'json_CartaGeologica_1',
+    layerName: 'layer_CartaGeologica_1',
+    pane: 'pane_CartaGeologica_1',
+    onEachFeature: pop_CartaGeologica_1,
+    style: style_CartaGeologica_1_0,
+});
+bounds_group.addLayer(layer_CartaGeologica_1);
+map.addLayer(layer_CartaGeologica_1);
+
+// pop up Litography
+function pop_Litografia_2(feature, layer) {
+    
+    // The popup content is in a string that contain html source of popup
+    // For use feature from geojson file use feature.properties['name_of_field'] and !== null ? autolinker.link(feature.properties['name_of_field'].toLocaleString()) : ''
+    // see example below this section
+    var popupContent = '<table>\
+        <tr><th style="text-align: center; vertical-align: middle;"> Sezione Sottile </th></tr>\
+        <tr><td style="text-align: center; vertical-align: middle;">\
+        \
+        <a href="https://it.wikipedia.org/wiki/File:Thin_section_scan_crossed_polarizers_Siilinjärvi_R636-105.90_edit.jpg"> \
+            <img\
+             src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/70/Thin_section_scan_crossed_polarizers_Siilinjärvi_R636-105.90_edit.jpg/320px-Thin_section_scan_crossed_polarizers_Siilinjärvi_R636-105.90_edit.jpg"\
+                alt="an image"\
+                style="width:128px" \
+            >\
+        </a>\
+        </td></tr></table>'; 
+         
+
+    // var popupContent = '<table>\
+    //         <tr>\
+    //         </tr>\
+    //         <tr>\
+    //             <th scope="row">Id</th>\
+    //             <td>' + (feature.properties['Id'] !== null ? autolinker.link(feature.properties['Id'].toLocaleString()) : '') + '</td>\
+    //         </tr>\
+    //     </table>'; 
+    layer.bindPopup(popupContent, {maxHeight: 400});
+}
+
+function style_Litografia_2_0() {
+    return {
+        pane: 'pane_Litografia_2',
+        radius: 30.0,
+        opacity: 1,
+        color: 'rgba(250,139,57,1.0)',
+        dashArray: '',
+        lineCap: 'butt',
+        lineJoin: 'miter',
+        weight: 4.0,
+        fill: true,
+        fillOpacity: 1,
+        fillColor: 'rgba(255,255,255,1.0)',
+        interactive: true,
+    }
+}
+function style_Litografia_2_1() {
+    return {
+        pane: 'pane_Litografia_2',
+        radius: 12.27272,
+        opacity: 1,
+        color: 'rgba(250,176,124,1.0)',
+        dashArray: '',
+        lineCap: 'butt',
+        lineJoin: 'miter',
+        weight: 1.0,
+        fill: true,
+        fillOpacity: 1,
+        fillColor: 'rgba(250,176,124,1.0)',
+        interactive: true,
+    }
+}
+map.createPane('pane_Litografia_2');
+map.getPane('pane_Litografia_2').style.zIndex = 402;
+map.getPane('pane_Litografia_2').style['mix-blend-mode'] = 'normal';
+var layer_Litografia_2 = new L.geoJson.multiStyle(json_Litografia_2, {
+    attribution: '',
+    interactive: true,
+    dataVar: 'json_Litografia_2',
+    layerName: 'layer_Litografia_2',
+    pane: 'pane_Litografia_2',
+    onEachFeature: pop_Litografia_2,
+    pointToLayers: [function (feature, latlng) {
+        var context = {
+            feature: feature,
+            variables: {}
+        };
+        return L.shapeMarker(latlng, style_Litografia_2_0(feature));
+    },function (feature, latlng) {
+        var context = {
+            feature: feature,
+            variables: {}
+        };
+        return L.shapeMarker(latlng, style_Litografia_2_1(feature));
+    },
+]});
+bounds_group.addLayer(layer_Litografia_2);
+map.addLayer(layer_Litografia_2);
+var osmGeocoder = new L.Control.Geocoder({
+    collapsed: true,
+    position: 'topleft',
+    text: 'Search',
+    title: 'Testing'
+}).addTo(map);
+
+// Section for add map control and navigation elements
+document.getElementsByClassName('leaflet-control-geocoder-icon')[0]
+.className += ' fa fa-search';
+document.getElementsByClassName('leaflet-control-geocoder-icon')[0]
+.title += 'Search for a place';
+var baseMaps = {};
+L.control.layers(baseMaps,{'<img src="legend/Litografia_2.png" /> Litografia': layer_Litografia_2,'Carta Geologica<br /><table><tr><td style="text-align: center;"><img src="legend/CartaGeologica_1_Arenariesilicoclastiche0.png" /></td><td>Arenarie silicoclastiche</td></tr><tr><td style="text-align: center;"><img src="legend/CartaGeologica_1_Argillemarnose1.png" /></td><td>Argille marnose</td></tr><tr><td style="text-align: center;"><img src="legend/CartaGeologica_1_CalcariaPorites2.png" /></td><td>Calcari a Porites</td></tr><tr><td style="text-align: center;"><img src="legend/CartaGeologica_1_Calcaribrecciatievaporitici3.png" /></td><td>Calcari brecciati evaporitici</td></tr><tr><td style="text-align: center;"><img src="legend/CartaGeologica_1_Detrito4.png" /></td><td>Detrito</td></tr><tr><td style="text-align: center;"><img src="legend/CartaGeologica_1_Frane5.png" /></td><td>Frane</td></tr><tr><td style="text-align: center;"><img src="legend/CartaGeologica_1_Gneissocchiadini6.png" /></td><td>Gneiss occhiadini</td></tr><tr><td style="text-align: center;"><img src="legend/CartaGeologica_1_Gneisstonalitici7.png" /></td><td>Gneiss tonalitici</td></tr><tr><td style="text-align: center;"><img src="legend/CartaGeologica_1_Paragneissmigmatitici8.png" /></td><td>Paragneiss migmatitici</td></tr><tr><td style="text-align: center;"><img src="legend/CartaGeologica_1_Skarns9.png" /></td><td>Skarns</td></tr><tr><td style="text-align: center;"><img src="legend/CartaGeologica_1_Spiaggeealluvioni10.png" /></td><td>Spiagge e alluvioni</td></tr><tr><td style="text-align: center;"><img src="legend/CartaGeologica_1_Terrazzimarini11.png" /></td><td>Terrazzi marini</td></tr><tr><td style="text-align: center;"><img src="legend/CartaGeologica_1_Trubi12.png" /></td><td>Trubi</td></tr><tr><td style="text-align: center;"><img src="legend/CartaGeologica_1_13.png" /></td><td></td></tr></table>': layer_CartaGeologica_1,"Google Sat": layer_GoogleSat_0,}).addTo(map);
+setBounds();
